d72b846ccf6edd99df7519d5eddd77b5a41fad0e
Leofront-end <ls171258@gmail.com>
2025-09-17 21:41:23 -0300

    feat: login

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let botaoLogin = document.querySelector('nav button')
+let modalLogin = document.querySelector('#modalLogin')
+
+botaoLogin.addEventListener('click', () => {
+    modalLogin.showModal()
+})

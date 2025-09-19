566997904bdcf19cf2068cd373d6a5ee14f9b613
Leofront-end <159971701+Leofront-end@users.noreply.github.com>
2025-09-19 12:15:25 -0300

    feat: login

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 let botaoLogin = document.querySelector('nav button')
 let modalLogin = document.querySelector('#modalLogin')
+let modalCadastro = document.querySelector('#modalCadastro')
+let botaoCadastro = document.querySelector('.hero button')
+let fecharModal = document.querySelectorAll('dialog span')
+
+fecharModal[0].addEventListener('click', () => modalLogin.close())
+fecharModal[1].addEventListener('click', () => modalCadastro.close())
+
+botaoCadastro.addEventListener('click', () => modalCadastro.showModal())
+
+function modalCadastros(e) {
+    e.preventDefault()
+    modalCadastro.showModal()
+}
+
 
 botaoLogin.addEventListener('click', () => {
     modalLogin.showModal()

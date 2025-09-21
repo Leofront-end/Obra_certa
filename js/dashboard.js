bbf96aa280c25f8cfbe68b2bb53c8751a2ce4ccf
gaab-sec <gabrieljrg2006@gmail.com>
2025-09-21 14:53:21 -0300

    feat: js pras setas e pontinhos do carrosel

---
+++ js/dashboard.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const adSlides = [ // textos dos slides
+        {content: 'Anúncio 1: Planeje sua obra com precisão!'},
+        {content: 'Anúncio 2: As melhores ferramentas para sua construção.'},
+        {content: 'Anúncio 3: Economize até 30% em materiais.'}
+    ];
+    
+    // setar índice inicial
+    let currentSlideIndex = 0;
+
+    const adContent = document.querySelector('.ad-content');
+    const arrows = document.querySelectorAll('.arrow');
+    const dots = document.querySelectorAll('.dot');
+
+    console.log('Elemento do conteúdo encontrado:', adContent);
+
+    function showSlide(index) {
+
+        console.log('Tentando exibir o texto:', adSlides[index].content);
+        // att texto do slide
+        adContent.textContent = adSlides[index].content;
+
+        // att pontinho ativo
+        dots.forEach(dot => dot.classList.remove('active'));
+        dots[index].classList.add('active');
+
+        // muda pro indice atual
+        currentSlideIndex = index;
+    };
+
+    arrows.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            if (arrow.textContent.includes('arrow_back_ios')) {
+                // indica q a seta pra esquerda volta um slide
+                currentSlideIndex = (currentSlideIndex > 0) ? currentSlideIndex - 1 : adSlides.length - 1;
+            } else {
+                // seta pra direita = passa um slide
+                currentSlideIndex = (currentSlideIndex < adSlides.length - 1) ? currentSlideIndex + 1 : 0;
+            }
+            showSlide(currentSlideIndex);
+        })
+    });
+
+    dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        showSlide(index);
+    });
+    });
+
+    showSlide(0);
+});

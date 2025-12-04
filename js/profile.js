0c21e3499cc9385c1052ac611be0b7e2040b8adc
Leofront-end <159971701+Leofront-end@users.noreply.github.com>
2025-12-04 09:12:49 -0300

    feat: editar perfil

--- js/profile.js
+++ js/profile.js
@@ -2,6 +2,8 @@ import { id } from "./id.js";
 
 let nomeInput = document.querySelector('#name')
 let emailInput = document.querySelector('#email')
+let botaoEditar = document.querySelector('.btn-edit')
+let modalEditar = document.querySelector('.project-modal')
 
 fetch(`https://obracerta-api.onrender.com/api/usuarios/${id}`, {
     method:"GET",
@@ -17,3 +19,28 @@ fetch(`https://obracerta-api.onrender.com/api/usuarios/${id}`, {
     .catch(() => {
         console.error('HOuve um erro')
     })
+
+botaoEditar.addEventListener('click', () => {
+    modalEditar.showModal()
+    let confirmarEdição = modalEditar.querySelector('.btn-primary') 
+    let sair = modalEditar.querySelector('.btn-secondary')
+    sair.addEventListener('click', () => modalEditar.close())
+
+    confirmarEdição.addEventListener('click', () => {
+        let dados = {
+            nome: `${nomeInput.value}`,
+            email: `${emailInput.value}`
+        }
+        fetch(`https://obracerta-api.onrender.com/api/usuarios/${id}`, {
+            method:"PUT",
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify(dados)
+        })
+            .then((response) => {
+                response.json()
+                modalEditar.close()
+            })
+    })
+})

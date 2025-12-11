4ccba3e6450f1f1ebd5854eb43d11c334f7b8166
Leofront-end <ls171258@gmail.com>
2025-12-11 18:28:52 -0300

    feat: criação de tarefa e listagem

--- js/project.js
+++ js/project.js
@@ -18,6 +18,7 @@ if(window.location.pathname == "/pages/projects/project.html") {
     }).then((data) => {
         titulo.textContent = data.titulo
     })
+
 } else if (window.location.pathname == "/pages/projects/tasks.html") {
     const queryString = window.location.search
 
@@ -29,7 +30,41 @@ if(window.location.pathname == "/pages/projects/project.html") {
     let itensFazer = formulario.querySelector('#itens')
     let selecao = formulario.querySelector('select')
 
-    formulario.addEventListener('submit', () => {
+    fetch(`https://obracerta-api.onrender.com/api/tarefas/projeto/${projectId}`, {
+        method: "GET",
+        headers: {
+            "Content-Type":"Application/json"
+        }
+    }).then((dados) => {
+        return dados.json()
+    }).then((resposta) => {
+        resposta.forEach(resposta => {
+            const listaDeTarefas = document.querySelector("ul")
+            const lista = document.createElement("li")
+            const titulo = document.createElement("p")
+            titulo.textContent = resposta.nome
+
+            const itensAFazer = document.createElement("p")
+            itensAFazer.textContent = resposta.itensAFazer
+
+            const prioridade = document.createElement("p")
+            prioridade.textContent = resposta.prioridade
+
+            const editar = document.createElement("button")
+            editar.textContent = "Editar"
+            editar.className = "btn-action btn-edit"
+
+            const excluir = document.createElement("button")
+            excluir.textContent = "Excluir"
+            excluir.className = "btn-action btn-delete"
+    
+            lista.append(titulo, itensAFazer, prioridade, editar, excluir)
+            listaDeTarefas.appendChild(lista)
+        });
+    })
+
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault()
         if (titulo.value == ""|| itensFazer.value == ""|| selecao.options[selecao.selectedIndex].value == "") {
             alert()
             return
@@ -40,6 +75,36 @@ if(window.location.pathname == "/pages/projects/project.html") {
             "prioridade": `${selecao.options[selecao.selectedIndex].value}`,
             "projetoId": projectId
         }
+        fetch(`https://obracerta-api.onrender.com/api/tarefas`, {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(dados)
+        }).then(() => {
+            const listaDeTarefas = document.querySelector("ul")
+            const lista = document.createElement("li")
+
+            const titulo = document.createElement("p")
+            titulo.textContent = dados.nome
+
+            const itensAFazer = document.createElement("p")
+            itensAFazer.textContent = dados.itensAFazer
+
+            const prioridade = document.createElement("p")
+            prioridade.textContent = dados.prioridade
+
+            const editar = document.createElement("button")
+            editar.textContent = "Editar"
+            editar.className = "btn-action btn-edit"
+
+            const excluir = document.createElement("button")
+            excluir.textContent = "Excluir"
+            excluir.className = "btn-action btn-delete"
+
+            lista.append(titulo, itensAFazer, prioridade, editar, excluir)
+            listaDeTarefas.appendChild(lista)
+        })
     })
 }
 
